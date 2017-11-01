49f728ed15c47dca3d566fe4ec08fcfc554c5efb
Justin Schwartzenberger <schwarty@gmail.com>
2017-11-01 09:31:31 -0700

    remove initial cart state

--- libs/cart-state/src/+state/cart.init.ts
+++ libs/cart-state/src/+state/cart.init.ts
@@ -1,13 +1,5 @@
 import { Cart } from './cart.interfaces';
 
 export const cartInitialState: Cart = {
-  items: [
-    {
-      id: "5",
-      name: "Flounder",
-      imageUrl: "/assets/images/flounder",
-      pricePerPound: 10.79,
-      quantity: 1
-    }
-  ]
+  items: []
 };
